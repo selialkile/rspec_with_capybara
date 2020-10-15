09af876d18abc871d7afd79d6ae5f8aa4f3b2992
Thiago Coutinho <thiago.coutinho@velocitycloud.com>
2020-10-15 16:34:26 +0200

    capybara test ok

---
+++ spec/capybara/hello_world_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Hello world', type: :system do
+
+  describe 'index page' do
+    it 'shows the right content' do
+
+      visit root_path
+
+      expect(page).to have_content('Testando com capybara')
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,3 +32,28 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+
+## set up client
+require 'selenium-webdriver'
+client = Selenium::WebDriver.for :chrome # see also following link.
+
+## set up options
+options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+  opts.args << '--headless'
+end
+
+# set up driver
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    http_client: client,
+    options: options)
+end
+
+# configure
+Capybara.configure do |config|
+  config.ignore_hidden_elements = true
+  config.default_max_wait_time = 3 #sec
+end
